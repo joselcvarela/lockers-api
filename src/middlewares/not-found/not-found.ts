82392c41f64c834c4aec7b4c8a00a4a7acffe3b9
Jose Varela <joselcvarela@gmail.com>
2024-11-24 21:24:50 +0000

    Fix Not Found middleware

--- src/middlewares/not-found/not-found.ts
+++ src/middlewares/not-found/not-found.ts
@@ -1,15 +1,18 @@
-import { respond } from "@/middlewares/respond/respond.js";
-
-async function start() {
+export async function start() {
   const events = await import("@/utils/events.js");
-  await events.on("routes.after", async (app) => {
-    app.use((_, res, next) => {
-      res.locals["status"] = 404;
-      res.locals["id"] = "not_found";
 
-      return next();
-    }, respond);
+  await events.on("routes.after", async (app) => {
+    const { respond } = await import("@/middlewares/respond/respond.js");
+    const { route } = await import("@/utils/route.js");
+
+    app.use(
+      route((_, res, next) => {
+        res.locals["status"] = 404;
+        res.locals["id"] = "not_found";
+
+        return next();
+      }),
+      respond,
+    );
   });
 }
-
-start();
