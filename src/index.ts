3db6587eb3379e86227b3dc4725818cfe55964b6
Jose Varela <joselcvarela@gmail.com>
2024-11-24 22:56:03 +0000

    Remove middlewares via events

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 async function start() {
   try {
-    await import("@/middlewares/index.js").then((m) => m.start());
-    await import("@/modules/index.js").then((m) => m.start());
-
     const { startServer } = await import("@/utils/start-server.js");
     await startServer();
+
+    await import("@/modules/index.js").then((m) => m.start());
   } catch (error) {
     if (error && error.toString) {
       const { sendSlackMessage } = await import("@/utils/send-slack-message.js");
--- src/middlewares/index.ts
+++
@@ -1,5 +0,0 @@
-export async function start() {
-  await import("@/middlewares/body-parser/body-parser.js").then((m) => m.start());
-  await import("@/middlewares/not-found/not-found.js").then((m) => m.start());
-  await import("@/middlewares/handle-error/handle-error.js").then((m) => m.start());
-}
