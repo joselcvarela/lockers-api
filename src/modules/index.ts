9550efbecc98ad19aaff21b34097ded6e9629a4b
Jose Varela <joselcvarela@gmail.com>
2024-11-24 23:27:54 +0000

    Remove unused files

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,3 @@
 export async function start() {
   await import("@/modules/prepare-rent/prepare-rent.js").then((m) => m.start());
-  await import("@/modules/rent-dropoff/rent-dropoff.js").then((m) => m.start());
 }
--- src/modules/prepare-rent/NoFreeLockersError.ts
+++
@@ -1,7 +0,0 @@
-import { HttpError } from "@/errors/HttpError.js";
-
-export class NoFreeLockersError extends HttpError {
-  constructor() {
-    super(`No free locks at the moment. Try later.`, 409);
-  }
-}
